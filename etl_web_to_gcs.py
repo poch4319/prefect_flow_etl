7838e4e19f2852307ed5ddaacdc876117c5c4e26
poch4319 <pochun.chang1991@gmail.com>
2023-02-02 10:24:23 +0800

    color

--- etl_web_to_gcs.py
+++ etl_web_to_gcs.py
@@ -19,11 +19,17 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 
 
 @task(log_prints=True)
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
     """Fix dtype issues"""
     print(df.head(2))
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    if color == 'green':
+        pickup = "lpep_pickup_datetime"
+        dropoff = "lpep_dropoff_datetime"
+    else:
+        pickup = "tpep_pickup_datetime"
+        dropoff = "tpep_dropoff_datetime"
+    df[pickup] = pd.to_datetime(df[pickup])
+    df[dropoff] = pd.to_datetime(df[dropoff])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -63,7 +69,7 @@ def etl_web_to_gcs(year: int, month: int, color: str) -> None:
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
     df = fetch(dataset_url)
-    df_clean = clean(df)
+    df_clean = clean(df, color)
     path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
 
